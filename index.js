31a107bc577f234f52fe6f092ac70eaed644c35a
Thales Lima <thales.bplima@gmail.com>
2022-02-05 03:03:48 -0300

    impl req2

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND = 404;
 const PORT = '3000';
 const TALKERFILE = './talker.json';
 
@@ -26,6 +27,22 @@ app.get('/talker', (_req, res) => {
       return parsedTalkers.length === 0
       ? res.status(HTTP_OK_STATUS).json([])
       : res.status(HTTP_OK_STATUS).json(parsedTalkers);
+    })
+    .catch((error) => {
+      console.error(`Não foi possível ler o arquivo ${TALKERFILE}. Erro: ${error}`);
+      process.exit(1);
+    });
+});
+
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  fs.readFile(TALKERFILE)
+    .then((talkers) => {
+      const parsedTalkers = JSON.parse(talkers);
+      const talkerById = parsedTalkers.find((talker) => talker.id === parseInt(id, 10));
+      return !talkerById
+      ? res.status(HTTP_NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' })
+      : res.status(HTTP_OK_STATUS).json(talkerById);
     })
     .catch((error) => {
       console.error(`Não foi possível ler o arquivo ${TALKERFILE}. Erro: ${error}`);
