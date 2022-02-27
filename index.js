1f4a31fc7f8b7b0525d86ea525524613c6fb0a96
Thales Lima <thales.bplima@gmail.com>
2022-02-27 03:26:45 -0300

    deleted trybe files

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK = 200;
 const PORT = '3000';
 
 const talkerRouter = require('./routes/talkerRoute');
@@ -12,11 +11,6 @@ const loginRouter = require('./routes/loginRoute');
 
 app.use('/talker', talkerRouter);
 app.use('/login', loginRouter);
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK).send();
-});
 
 app.listen(PORT, () => {
   console.log(`Online na porta ${PORT}`);
